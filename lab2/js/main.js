07eb2e157123dee1e46bad03503bce7f11483d04
Ashbin-M-Abraham <133525700+Ashbin-M-Abraham@users.noreply.github.com>
2024-03-06 11:40:46 -0500

    constuctor

--- lab2/js/main.js
+++ lab2/js/main.js
@@ -1,1 +1,28 @@
 const output = document.getElementById('output');
+
+// creating constuctor
+
+function HotWheel(name,year,color,numberOfWheels,companyName,collectionName,toyNumber,barcode,CountryMade,noOfDoors,price){
+    this.name =name;
+    this.year =year;
+    this.color= color; 
+    this.numberOfWheels = numberOfWheels;  
+    this.companyName =companyName
+    this.collectionName = collectionName ;
+    this.toyNumber = toyNumber;
+    this.barcode = barcode;
+    this.CountryMade = CountryMade;
+    this.noOfDoors = noOfDoors;
+    this.price = price;
+    this.describe = function(){
+        let description =`${this.name} is a HotWheel toy produced by ${this.companyName}.
+         It was released in ${this.year} as part of the ${this.collectionName} collection.
+          The toy is ${this.color} in color and has ${this.numberOfWheels} wheels. 
+          It was manufactured in ${this.CountryMade}. 
+          It has ${this.noOfDoors} doors and is identified by toy number ${this.toyNumber}
+           and barcode ${this.barcode}. The price is ${this.price}.`;
+        return  description;
+    }
+
+}
+
