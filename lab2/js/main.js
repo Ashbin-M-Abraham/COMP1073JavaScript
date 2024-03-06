c81b0fc86ae2832304fae55ade3bfd9c83541842
Ashbin-M-Abraham <133525700+Ashbin-M-Abraham@users.noreply.github.com>
2024-03-06 11:42:51 -0500

    output

--- lab2/js/main.js
+++ lab2/js/main.js
@@ -27,3 +27,5 @@ function HotWheel(name,year,color,numberOfWheels,companyName,collectionName,toyN
 }
 
 const HotWheel1 = new HotWheel("punk Rod",2023,"black and orange",4,"Hot Wheeks","Rod Squad","HRY48",2708412013,"Malasiya",2,2.99)
+
+output.textContent = album2.describe();
